552fa89cdcb410f5d758481dee5f853eaec6a9f2
Steve Wang <wanghongzecn@qq.com>
2019-09-10 09:37:49 +0800

    Create MaximumDepthofBinaryTree.java

---
+++ MaximumDepthofBinaryTree.java
@@ -0,0 +1,17 @@
+package LeetCode;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+// 104.MaximumDepthofBinaryTree
+public class MaximumDepthofBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        else return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+    }
+}
