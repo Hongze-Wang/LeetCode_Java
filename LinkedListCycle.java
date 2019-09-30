3c515285f323104e7e982ed979249016510fcb77
Steve Wang <wanghongzecn@qq.com>
2019-09-30 15:09:26 +0800

    Update LinkedListCycle.java

--- LinkedListCycle.java
+++ LinkedListCycle.java
@@ -32,12 +32,34 @@
 // }
 
 // two pinters method
+// fast is twice the speed as the slow
+// so when fast == slow, they are at the same node in the circle
 public class Solution {
     public boolean hasCycle(ListNode head) {
         if(head == null || head.next == null) return false;
         
         ListNode slow = head;
         ListNode fast = head.next;
+        
+        while(slow != fast) {
+            if(fast == null || fast.next == null) return false;
+            
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
+
+// The recommend implement way
+// You will see their difference in the 142 Linked List Cycle II
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null) return false;
+        
+        ListNode slow = head.next;
+        ListNode fast = head.next.next;
         
         while(slow != fast) {
             if(fast == null || fast.next == null) return false;
