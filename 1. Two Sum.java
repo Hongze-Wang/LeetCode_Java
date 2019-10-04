2c9ff3469c3975e7afb0e5dc2df119952b09e7f9
Steve Wang <wanghongzecn@qq.com>
2019-10-04 11:42:39 +0800

    Create 1. Two Sum.java

---
+++ 1. Two Sum.java
@@ -0,0 +1,22 @@
+package LeetCode;
+import java.util.HashMap;
+import java.util.Map;
+// 1. Two Sum
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map <Integer, Integer> result = new HashMap<>();
+        int []indexes = {0 ,1};
+
+        if(nums.length > 2) {
+            for(int i=0; i<nums.length; i++) {
+                if(result.containsKey(nums[i])) {
+                    indexes[0] = result.get(nums[i]);
+                    indexes[1] = i;
+                }
+                int remainder = target - nums[i];
+                result.put(remainder, i);
+            }
+        }
+        return indexes;
+    }
+}
