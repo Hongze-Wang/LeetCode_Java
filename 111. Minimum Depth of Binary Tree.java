7f27f0baea6245111baa04e369205fbbbfc2005e
Steve Wang <wanghongzecn@qq.com>
2019-10-04 11:42:54 +0800

    Create 111. Minimum Depth of Binary Tree.java

---
+++ 111. Minimum Depth of Binary Tree.java
@@ -0,0 +1,28 @@
+package LeetCode;
+
+// 111. Minimum Depth of Binary Tree
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class MinimumDepthofBinaryTree {
+    public int minDepth(TreeNode root) {
+        return depth(root);
+    }
+    private int depth(TreeNode root) {
+        if(root == null ) return 0;
+        if(root.left == null && root.right != null) {
+            return depth(root.right) + 1;
+        }
+        if(root.left != null && root.right == null) {
+            return depth(root.left) + 1;
+        }
+        return Math.min(depth(root.left), depth(root.right)) + 1;
+    }
+}
