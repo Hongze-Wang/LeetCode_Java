44d7fd786f8ee6a8232bed46300e03aaa485c6b9
Steve Wang <wanghongzecn@qq.com>
2019-10-05 11:13:40 +0800

    Create 168. Excel Sheet Column Title.java

---
+++ 168. Excel Sheet Column Title.java
@@ -0,0 +1,24 @@
+// 168. Excel Sheet Column Title
+
+// 本质上是进制转换问题 10进制转26进制
+// 辗转相除的思想
+// when rem != 26 "n -= rem; can be saved, but rem can be 26
+// So it only handle when rem = 26
+class Solution {
+    public String convertToTitle(int n) {
+        String column = "";
+        char letter;
+        int rem;
+        while(n != 0) {
+            rem = n % 26;
+            if(rem == 0) {
+                rem = 26;
+            }
+            letter = (char) (rem+64);
+            column = letter + column;
+            n -= rem;  // n - 26
+            n /= 26;
+        }
+        return column;
+    }
+}
