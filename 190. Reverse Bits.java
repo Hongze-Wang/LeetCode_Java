f71f6422439f66df0cc541040aeae023e2af136f
Steve Wang <wanghongzecn@qq.com>
2019-10-08 16:21:19 +0800

    Update 190. Reverse Bits.java

--- 190. Reverse Bits.java
+++ 190. Reverse Bits.java
@@ -27,3 +27,18 @@ public class Solution {
         return res;
     }
 }
+
+public class Solution {
+    public int reverseBits(int n) {
+        int res = 0;
+        for(int i = 0; i < 32; i++) {
+            res <<= 1;
+
+            if((n&1) ==1) {
+                res |= 1;
+            }
+            n >>= 1;
+        }
+        return res;
+    }
+}
