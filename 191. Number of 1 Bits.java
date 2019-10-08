58ee7a8a1fb6e07b588ad60ece0258be6e85e9dd
Steve Wang <wanghongzecn@qq.com>
2019-10-08 16:36:42 +0800

    Update 191. Number of 1 Bits.java

--- 191. Number of 1 Bits.java
+++ 191. Number of 1 Bits.java
@@ -1,6 +1,6 @@
 // 191. Number of 1 Bits
 
-// 最直观解法
+// 最直观解法 100% faster(0 ms) 不稳定 有时候跑 55%(1 ms)
 public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
@@ -38,5 +38,22 @@ public class Solution {
             n &= (n - 1); // 能执行一次该语句 代表有一个1 详见https://leetcode.com/problems/number-of-1-bits/solution/
         }
         return count;
+    }
+}
+
+// 递归形式 效率与上面·相差不大
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        if(n == 0) return 0;
+        return hammingWeight(n & (n - 1)) + 1;
+    }
+}
+
+// 依然不快
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        return Integer.bitCount(n ^ 0);
     }
 }
