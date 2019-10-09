0474c9d4764d634c011b4a2f716a3b9b81334b3e
Steve Wang <wanghongzecn@qq.com>
2019-10-09 16:24:56 +0800

    Update 202. Happer Number.cpp

--- 202. Happer Number.cpp
+++ 202. Happer Number.cpp
@@ -2,6 +2,8 @@
 
 // Square Sum < 1000
 // Because int 10 digits 10 * (9^2) = 810 
+// 本质上是把以算过的map到了一个布尔数组里 以这个布尔数组里存的元素作为循环的终止条件
+
 class Solution {
 public:
     int calSquareSum(int n) {
@@ -14,7 +16,7 @@ public:
     }
     bool isHappy(int n) {
         vector<bool> seen = vector<bool>(1000, false);
-        n = calSquareSum(n);
+        n = calSquareSum(n); // 算一次 就能确保 n<1000
 
         while(!seen[n]) {
             if(n == 1) return true;
