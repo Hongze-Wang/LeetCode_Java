b147e0616f0c18934cbdbc1344ad3d6ef64d1853
Steve Wang <wanghongzecn@qq.com>
2020-07-06 16:17:14 +0800

    Create 458.Poor Pigs.cpp

---
+++ 458.Poor Pigs.cpp
@@ -0,0 +1,8 @@
+// 458.Poor Pigs
+
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        return ceil(log(buckets) / log(minutesToTest / minutesToDie + 1));
+    }
+};
