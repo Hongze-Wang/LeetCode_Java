92566982a4e8798711b2a59e72b01c3c95c7e349
Steve Wang <wanghongzecn@qq.com>
2020-07-07 19:42:36 +0800

    Update 33. Search in Rotated Sorted Array.java

--- 33. Search in Rotated Sorted Array.java
+++ 33. Search in Rotated Sorted Array.java
@@ -34,3 +34,33 @@ class Solution {
         search(nums, mid, right, target);
     }
 }
+
+// 33. Search in Rotated Sorted Array
+// Same solution as 81. Search in Rotated Sorted Array II.java
+
+class Solution {
+    public int search(int[] nums, int target) {
+        if(nums == null || nums.length == 0) return -1;
+        if(nums.length == 0 && nums[0] == target) return 0;
+        
+        int low=0, high=nums.length-1, mid;
+        while(low <= high) {
+            mid = low + (high-low) / 2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < nums[high]) {
+                if(nums[mid] < target && nums[high] >= target) {
+                    low = mid+1;
+                } else {
+                    high = mid-1;
+                }
+            } else {
+                if(nums[mid] > target && nums[low] <= target) {
+                    high = mid-1;
+                } else {
+                    low = mid+1;
+                }
+            }
+        }
+        return -1;
+    }
+}
