57246cd4fd95fc78bb64d2449b496dfe3dcfee3d
Steve Wang <wanghongzecn@qq.com>
2020-07-09 11:16:28 +0800

    Create 344. Reverse String.cpp

---
+++ 344. Reverse String.cpp
@@ -0,0 +1,10 @@
+// 344. Reverse String
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        for(int i=0, n=s.size(); i < n/2; i++) {
+            swap(s[i], s[n-i-1]);
+        }
+    }
+};
