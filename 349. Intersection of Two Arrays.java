ece4099156973a9b3aab8282cc29a3c204cda2a3
Steve Wang <wanghongzecn@qq.com>
2020-07-10 10:23:11 +0800

    Create 349. Intersection of Two Arrays.java

---
+++ 349. Intersection of Two Arrays.java
@@ -0,0 +1,17 @@
+// 349. Intersection of Two Arrays
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set1 = new HashSet();
+        for(Integer n: nums1) set1.add(n);
+        HashSet<Integer> set2 = new HashSet();
+        for(int n: nums2) set2.add(n);
+        
+        set1.retainAll(set2);
+        
+        int[] res = new int[set1.size()];
+        int i=0;
+        for(int s: set1) res[i++] = s;
+        return res;
+    }
+}
