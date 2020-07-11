82fb90a75c92e73ceb9d4a331bb584749543ac29
Steve Wang <wanghongzecn@qq.com>
2020-07-11 10:37:24 +0800

    Create 213. House Robber II.cpp

---
+++ 213. House Robber II.cpp
@@ -0,0 +1,19 @@
+// 213. House Robber II
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+        
+        vector<int> dp1(n+1, 0);
+        vector<int> dp2(n+1, 0);
+        
+        for(int i=2; i<n+1; i++) {
+            dp1[i] = max(dp1[i-1], nums[i-2]+dp1[i-2]);
+            dp2[i] = max(dp2[i-1], nums[i-1]+dp2[i-2]);
+        }
+        return max(dp1[n], dp2[n]);
+    }
+};
