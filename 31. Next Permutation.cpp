10a2de39fe5355dab759e2bb09b91398778287d4
Steve Wang <wanghongzecn@qq.com>
2020-07-22 20:15:42 +0800

    Create 31. Next Permutation.cpp

---
+++ 31. Next Permutation.cpp
@@ -0,0 +1,11 @@
+// 31. Next Permutation
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        if(next_permutation(nums.begin(), nums.end())) {
+            return;
+        } else {
+            sort(nums.begin(), nums.end());
+        }
+    }
+};
