645cf8d0956fb2162948cea2060473f999e0a7db
Steve Wang <wanghongzecn@qq.com>
2020-08-17 21:04:52 +0800

    Update 55. Jump Game.java

--- 55. Jump Game.java
+++ 55. Jump Game.java
@@ -89,40 +89,27 @@
 //     }
 // }
 
-// Approach 2: Dynamic Programming Top-down
-// It relies on the observation that once we determine that a certain index is good / bad, this result will never change. 
-// This means that we can store the result and not need to recompute it every time.
-// memoization
-
-// enum Index {
-//     GOOD, BAD, UNKNOW
-// }
-
+// Top down dp
 // public class Solution {
-//     Index[] memo;
-    
-//     public boolean canJumpFromPosition(int pos, int[] nums) {
-//         if(memo[pos] != Index.UNKNOW) {
-//             return memo[pos] == Index.GOOD ? true : false;
+//     public boolean canJumpFromPostion(int pos, int[] nums, int[] memo) {
+//         if(memo[pos] != 0) {
+//             return memo[pos] == 1 ? true : false;
 //         }
 //         int fur = Math.min(pos+nums[pos], nums.length-1);
-//         for(int next=pos+1; next<=fur; next++) {
-//             if(canJumpFromPosition(next, nums)) {
-//                 memo[pos] = Index.GOOD;
-//                 return true;   
+//         for(int next=pos+1; next <= fur; next++) {
+//             if(canJumpFromPostion(next, nums, memo)) {
+//                 memo[pos] = 1;
+//                 return true;
 //             }
 //         }
-//         memo[pos] = Index.BAD;
+//         memo[pos] = 2;
 //         return false;
 //     }
-    
-//     public boolean canJump(int[ ] nums) {
-//         memo = new Index[nums.length];
-//         for(int i=0; i<memo.length; i++) {
-//             memo[i] = Index.UNKNOW;
-//         }
-//         memo[memo.length-1] = Index.GOOD;
-//         return canJumpFromPosition(0, nums);
+
+//     public boolean canJump(int[] nums) {
+//         int[] memo = new int[nums.length];
+//         memo[nums.length-1] = 1;
+//         return canJumpFromPostion(0, nums, memo);
 //     }
 // }
 
@@ -151,6 +138,25 @@
 //             }
 //         }
 //         return memo[0] == Index.GOOD;
+//     }
+// }
+
+// Bottom up dp
+// public class Solution {
+//     public boolean canJump(int[] nums) {
+//         int[] memo = new int[nums.length];
+//         memo[nums.length-1] = 1;
+        
+//         for(int i=nums.length-2; i>=0; i--) {
+//             int fur = Math.min(i+nums[i], nums.length-1);
+//             for(int j=i+1; j<=fur; j++) {
+//                 if(memo[j] == 1) {
+//                     memo[i] = 1;
+//                     break;
+//                 }
+//             }
+//         }
+//         return memo[0] == 1;
 //     }
 // }
 
