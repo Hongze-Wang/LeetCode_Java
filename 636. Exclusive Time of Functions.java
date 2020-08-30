90e8b241ba6b2e038a4f87505925069087792552
Steve Wang <wanghongzecn@qq.com>
2020-08-30 20:46:23 +0800

    Update 636. Exclusive Time of Functions.java

--- 636. Exclusive Time of Functions.java
+++ 636. Exclusive Time of Functions.java
@@ -53,3 +53,27 @@ class Solution {
         return res;
     }
 }
+
+class Solution {
+    public int[] exclusiveTime(int n, List<String> logs) {
+        Stack<Integer> stack = new Stack();
+        int res[] = new int[n];
+        String[] s = logs.get(0).split(":");
+        stack.push(Integer.parseInt(s[0]));
+        int i = 0, prev = Integer.parseInt(s[2]);
+        while(i < logs.size()) {
+            s = logs.get(i).split(":");
+            if(s[1].equals("start")) {
+                res[stack.peek()] += Integer.parseInt(s[2]) - prev;
+                stack.push(Integer.parseInt(s[0]));
+                prev = Integer.parseInt(s[2]);
+            } else {
+                res[stack.peek()] += Integer.parseInt(s[2]) - prev + 1; // end at the end of Integer.parseInt(s[2]) + 1
+                stack.pop();
+                prev = Integer.parseInt(s[2]) + 1; // begin at the start of Integer.parseInt(s[2]) + 1
+            }
+            i++;
+        }
+        return res;
+    }
+}
