f9a34183b5484b847581fbc50d2f14da93520560
Steve Wang <wanghongzecn@qq.com>
2020-09-30 11:46:46 +0800

    Update 144. Binary Tree Preorder Traversal.java

--- 144. Binary Tree Preorder Traversal.java
+++ 144. Binary Tree Preorder Traversal.java
@@ -29,3 +29,25 @@ class Solution {
         return res;
     }
 }
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList();
+        if(root == null) return res;
+        
+        Stack<TreeNode> stack = new Stack();
+        stack.push(root);
+        while(!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            res.add(cur.val);
+            
+            if(cur.right != null) {
+                stack.push(cur.right);
+            }
+            if(cur.left != null) {
+                stack.push(cur.left);
+            }
+        }
+        return res;
+    }
+}
