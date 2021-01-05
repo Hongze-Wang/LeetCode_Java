fcf45b57e74ce2c0762d4da1c165d67a2ee56687
Steve Wang <wanghongzecn@qq.com>
2021-01-05 22:06:28 +0800

    Update 190. Reverse Bits.java

--- 190. Reverse Bits.java
+++ 190. Reverse Bits.java
@@ -29,14 +29,12 @@ public class Solution {
 }
 
 public class Solution {
+    // you need treat n as an unsigned value
     public int reverseBits(int n) {
         int res = 0;
-        for(int i = 0; i < 32; i++) {
-            res <<= 1;
-
-            if((n&1) ==1) {
-                res |= 1;
-            }
+        
+        for(int i=0; i<32; i++) {
+            res = res << 1 | (n & 1);
             n >>= 1;
         }
         return res;
