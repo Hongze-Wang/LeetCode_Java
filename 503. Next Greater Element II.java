fce28b3b926a231516413ebedafa6b58e816586b
Steve Wang <wanghongzecn@qq.com>
2021-01-08 19:36:47 +0800

    Update 503. Next Greater Element II.java

--- 503. Next Greater Element II.java
+++ 503. Next Greater Element II.java
@@ -1,5 +1,45 @@
 // 503. Next Greater Element II
 
+// brute force - accepted
+// class Solution {
+//     public int[] nextGreaterElements(int[] nums) {
+//         int[] res = new int[nums.length];
+//         int[] copy = new int[nums.length * 2];
+//         System.arraycopy(nums, 0, copy, 0, nums.length);
+//         System.arraycopy(nums, 0, copy, nums.length, nums.length);
+        
+//         for(int i=0; i<nums.length; i++) {
+//             res[i] = -1; // 初始化为-1 防止找不到
+//             for(int j=i+1; j<copy.length; j++) {
+//                 if(copy[j] > nums[i]) {
+//                     res[i] = copy[j];
+//                     break;
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// }
+
+// optimized bf
+// use % to avoid extra space
+
+// class Solution {
+//     public int[] nextGreaterElements(int[] nums) {
+//         int[] res = new int[nums.length];
+//         for(int i=0; i<nums.length; i++) {
+//             res[i] = -1;
+//             for(int j=i+1; j<2*nums.length; j++) {
+//                 if(nums[j % nums.length] > nums[i]) {
+//                     res[i] = nums[j % nums.length];
+//                     break;
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// }
+
 // 分析题目不难看出解法要使用单调栈
 // 找下一个更大元素可以通过从右向左构造递增来实现 递增栈保存了比当前元素更大的元素 如果当前元素最大 则递增栈为空
 // 从右向左构造递增栈相当于从左向右找下一个最大的数 如果这个方向找不到可能要尝试从右向左找
